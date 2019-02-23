6e06206359fa5a591a1eb362a658f6fdd2e9722d
eserhin <47678732+eserhin@users.noreply.github.com>
2019-02-23 17:28:18 +0300

    Update index.js
    hard

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var zeros=9999999999, currently;
+  for (var g = 2; g <= base; g++){
+      var degree = 0
+      while (base % g === 0){
+        base /= g;
+        degree++;
+      } 
+      var kolvo = 0;
+      p=g;
+      while (number / p > 0){
+        kolvo += Math.floor(number / p);
+        p*=g; 
+      }
+      currently=Math.floor(kolvo/degree);
+      if(zeros>currently) zeros=currently;     	
+}
+  return zeros;
 }
